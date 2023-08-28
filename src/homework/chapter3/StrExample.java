79c109ae88083633ca6b03df9599c14f88f230d8
Ashot Khachatryan <khachatryan.ashot14@gmail.com>
2023-08-28 20:42:31 +0400

    StrExamle

---
+++ src/homework/chapter3/StrExample.java
@@ -0,0 +1,9 @@
+package homework.chapter3;
+
+public class StrExample {
+    public static void main(String[] args) {
+        char[] name = {'A', 'S', 'H', 'O', 'T'};
+        String nameStr = new String(name);
+        System.out.print(new String (name));
+    }
+}
