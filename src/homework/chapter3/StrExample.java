f5d10dbfac0c148d4ec7562edd60e310eaba3f11
Ashot <143201856+Ashot66@users.noreply.github.com>
2023-08-28 20:45:10 +0400

    Delete StrExample.java

--- src/homework/chapter3/StrExample.java
+++
@@ -1,9 +0,0 @@
-package homework.chapter3;
-
-public class StrExample {
-    public static void main(String[] args) {
-        char[] name = {'A', 'S', 'H', 'O', 'T'};
-        String nameStr = new String(name);
-        System.out.print(new String (name));
-    }
-}
