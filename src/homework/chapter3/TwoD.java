6fe339a865fc9fb269ad2700aae3185ab2e6533f
Ashot Khachatryan <khachatryan.ashot14@gmail.com>
2023-08-29 18:43:19 +0400

    StrExamle

---
+++ src/homework/chapter3/TwoD.java
@@ -0,0 +1,17 @@
+package homework.chapter3;
+
+public class TwoD {
+    public static void main(String[] args) {
+        int twoD[][] = new int[4][5];
+
+        int i, j, k = 0;
+        for (i = 0; i < 4; i++)
+            for (j = 0; j < 5; j++) {
+
+                System.out.print((twoD[i][j] = k));
+                k++;
+            }
+        System.out.println();
+
+    }
+}
